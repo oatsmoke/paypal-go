da37742f62018f1831f389f793e7cd43ca50064a
Roman Nozhenko <oatsmoke@bk.ru>
2024-07-15 13:57:26 +0600

    fix time

--- model/capture.go
+++ model/capture.go
@@ -1,7 +1,5 @@
 package model
 
-import "time"
-
 type Capture struct {
 	Id                        string                     `json:"id,omitempty"`
 	Amount                    *Amount                    `json:"amount,omitempty"`
@@ -14,7 +12,7 @@ type Capture struct {
 	FinalCapture              bool                       `json:"final_capture,omitempty"`
 	SellerReceivableBreakdown *SellerReceivableBreakdown `json:"seller_receivable_breakdown,omitempty"`
 	InvoiceId                 string                     `json:"invoice_id,omitempty"`
-	CreateTime                time.Time                  `json:"create_time,omitempty"`
-	UpdateTime                time.Time                  `json:"update_time,omitempty"`
+	CreateTime                string                     `json:"create_time,omitempty"`
+	UpdateTime                string                     `json:"update_time,omitempty"`
 	Links                     []*Link                    `json:"links,omitempty"`
 }
