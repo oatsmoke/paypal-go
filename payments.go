abacdb35392b2a61a96cd3e54c8cfb5157ec7b06
Roman Nozhenko <oatsmoke@bk.ru>
2024-07-15 14:57:26 +0600

    add ShowRefundDetails

--- payments.go
+++ payments.go
@@ -29,3 +29,25 @@ func (c *Client) RefundCapturedPayment(ctx context.Context, captureId string) (*
 
 	return out, nil
 }
+
+func (c *Client) ShowRefundDetails(ctx context.Context, refundId string) (*model.Capture, error) {
+	const fn = "ShowRefundDetails"
+
+	url := fmt.Sprintf("%s/v2/payments/refunds/%s", c.URL, refundId)
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := c.SendWithAuth(ctx, req, http.StatusOK, fn)
+	if err != nil {
+		return nil, err
+	}
+
+	out := new(model.Capture)
+	if err := json.Unmarshal(res, &out); err != nil {
+		return nil, err
+	}
+
+	return out, nil
+}
